1b792f5e77e3407b5ced8a9f7b2896d5cb65da41
Ansh-0815 <agarwalansh005@gmail.com>
2025-10-16 14:33:18 +0530

    server chl gya

--- Examsystem/backend/teacher_assessment/settings.py
+++ Examsystem/backend/teacher_assessment/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '..', 'frontend', 'static','css'), os.path.join(BASE_DIR, '..', 'frontend', 'static','js'), os.path.join(BASE_DIR, '..', 'frontend', 'static','images')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- Online-exam/Examsystem/backend/questions/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Question
-# Create your views here.
-
-def home(request):
-    questions = Question.objects.all().order_by('-created_at')
-    return render(request, 'templates/index.html', {'questions': questions})
